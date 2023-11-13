9494bb0ba887866ff524619ee1f1cfca1c007260
AnhQuanTrl <anhquan.tranle0601@gmail.com>
2023-11-13 17:01:42 +0700

    feat: organize folder structure

--- flake.nix
+++ flake.nix
@@ -32,9 +32,7 @@
   outputs = { self, nixpkgs, nixos-wsl, home-manager, nix-index-database, flake-utils, sops-nix, ... } @ inputs:
     let
       systems = [ "x86_64-linux" ];
-      system = "x86_64-linux";
       lib = nixpkgs.lib;
-      pkgs = nixpkgs.legacyPackages.${system};
       dotfilesLib = {
       	runtimePath = runtimeRoot: path:
         let
@@ -44,37 +42,39 @@
                 "${pathStr} does not start with ${rootStr}";
         runtimeRoot + lib.removePrefix rootStr pathStr;
       };
+      nixosSystem = modules: nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = {
+          inherit inputs;
+        };
+        inherit modules;
+      };
+      homeManagerConfiguration = modules: home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages."x86_64-linux";
+        extraSpecialArgs = {
+          inherit inputs;
+          inherit system;
+          inherit dotfilesLib;
+        };
+        inherit modules;
+      };
     in {
       nixosConfigurations = {
-        orion = nixpkgs.lib.nixosSystem {
-          inherit system;
-
-          modules = [
-            nixos-wsl.nixosModules.wsl
-            ./wsl.nix
-          ];
-
-          specialArgs = {
-            inherit inputs;
-          };
-        };
+        orion = nixosSystem [ nixos-wsl.nixosModules.wsl ./hosts/orion ];
+        lyra = nixosSystem [ hosts/lyra ];
       };
 
       homeConfigurations = {
-        "betelgeuse@orion" = home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-
-          modules = [
+        "betelgeuse@orion" = homeManagerConfiguration [
       	    nix-index-database.hmModules.nix-index
             sops-nix.homeManagerModules.sops
-	          ./home.nix
-	        ];
-          extraSpecialArgs = {
-            inherit inputs;
-            inherit system;
-            inherit dotfilesLib;
-          };
-        };
+	    ./home.nix
+	];
+        "vega@lyra" = homeManagerConfiguration [
+          nix-index-database.hmModules.nix-index
+          sops-nix.homeManagerModules.sops
+	  ./home.nix
+        ];
       };
     } // flake-utils.lib.eachSystem systems (system: import ./shells nixpkgs.legacyPackages.${system});
 }
